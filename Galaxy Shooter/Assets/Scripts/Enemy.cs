b582b40e8d6d1661ba5b9535689576cdef3f31c0
Ignacio Sada Solomon <ignacio.sadasolomon@mail.mcgill.ca>
2020-03-22 20:20:07 -0400

    Created an attempt on Enemy AI (crash apparent)

---
+++ Galaxy Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float _r = Random.Range(-9f, 9f);
+    [SerializeField]
+    private float _speed = 4.0f;
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_enemyPrefab, new Vector3(_r, 7f, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move down at x m/s
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if (transform.position.y <= -6) { transform.position = new Vector3(_r, 7f, 0); }
+    }
+}
