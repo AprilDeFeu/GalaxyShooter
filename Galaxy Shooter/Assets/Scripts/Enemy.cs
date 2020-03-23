dc43e5fd6d21f85942b3c05e585e1afd4b047c58
Ignacio Sada Solomon <ignacio.sadasolomon@mail.mcgill.ca>
2020-03-23 03:26:16 -0400

    Enemy stops in collision with laser, also added enemy sinusoidal movement

--- Galaxy Shooter/Assets/Scripts/Enemy.cs
+++ Galaxy Shooter/Assets/Scripts/Enemy.cs
@@ -4,22 +4,94 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private float _r = Random.Range(-9f, 9f);
     [SerializeField]
-    private float _speed = 4.0f;
+    private enum Type{ One, Two, Three, Four, Five}
+    [SerializeField]
+    private Type level;
+    [SerializeField]
+    private float _speed = 2.0f;
     [SerializeField]
     private GameObject _enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(_enemyPrefab, new Vector3(_r, 7f, 0), Quaternion.identity);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MovementL2();
+        switch (level)
+        {
+            case Type.One:
+                MovementL1();
+                break;
+            case Type.Two:
+                MovementL2();
+                break;
+            case Type.Three:
+                MovementL3();
+                break;
+            case Type.Four:
+                MovementL4();
+                break;
+            case Type.Five:
+                MovementL5();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string player = "Player";
+        string laser = "Laser(Clone)";
+        if (other.transform.name.Equals(player)) Destroy(other) ;
+        if (other.transform.name.Equals(laser))
+        {
+            Destroy(this);
+            Destroy(other);
+        }
+    }
+
+    void MovementL1()
+    {
         // Move down at x m/s
-        transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        if (transform.position.y <= -6)  transform.position = new Vector3(_r, 7f, 0); 
+        Vector3 wobble = new Vector3(4 * Mathf.Sin(8 * Time.time), 0, 0);
+        transform.Translate((Vector3.down + wobble) * _speed * Time.deltaTime);
+        if (transform.position.y <= -6) transform.position = new Vector3(Random.Range(-11.0f, 11.0f), 7f, 0);
+    }
+
+    void MovementL2()
+    {
+        // Move down at x m/s
+        Vector3 wobble = new Vector3(4 * Mathf.Sin(4 * Time.time), 0, 0);
+        transform.Translate((Vector3.down + wobble) * _speed * Time.deltaTime);
+        if (transform.position.y <= -6) transform.position = new Vector3(Random.Range(-9.0f, 9.0f), 7f, 0);
+    }
+
+
+    void MovementL3()
+    {
+        // Move down at x m/s
+        Vector3 wobble = new Vector3(8 * Mathf.Sin(6 * Time.time), 0, 0);
+        transform.Translate((Vector3.down + wobble) * _speed * Time.deltaTime);
+        if (transform.position.y <= -6) transform.position = new Vector3(Random.Range(-9.0f, 9.0f), 7f, 0);
+    }
+
+    void MovementL4()
+    {
+        // Move down at x m/s
+        Vector3 wobble = new Vector3(8 * Mathf.Sin(12 * Time.time), 0, 0);
+        transform.Translate((Vector3.down + wobble) * _speed * Time.deltaTime);
+        if (transform.position.y <= -6) transform.position = new Vector3(Random.Range(-9.0f, 9.0f), 7f, 0);
+    }
+
+    void MovementL5()
+    {
+        // Move down at x m/s
+        Vector3 wobble = new Vector3(4 * Mathf.Sin(3 * Time.time), 0, 0);
+        transform.Translate((Vector3.down + wobble) * 0.5f * _speed * Time.deltaTime);
+        if (transform.position.y <= -6) transform.position = new Vector3(Random.Range(-9.0f, 9.0f), 7f, 0);
     }
 }
